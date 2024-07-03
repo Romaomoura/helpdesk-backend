5020a67ac3b1ee0c5cf4435aa066a502c3e6848c
Romão Moura <c.romaomoura@gmail.com>
2024-07-03 10:36:10 -0300

    Anotações JPA para classes de dominio.

--- helpdesk/src/main/java/com/devromaomoura/helpdesk/domain/Tecnico.java
+++ helpdesk/src/main/java/com/devromaomoura/helpdesk/domain/Tecnico.java
@@ -1,17 +1,25 @@
 package com.devromaomoura.helpdesk.domain;
 
+import com.devromaomoura.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+@Entity
+public class Tecnico extends Pessoa {
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
